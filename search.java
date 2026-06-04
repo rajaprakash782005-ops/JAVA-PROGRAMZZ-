db7f65cdc7a5a0d0564f97fe63d601960f955547
Rajaprakash R <rajaprakash782005@gmail.com>
2026-06-04 10:11:33 +0530

    Add files via upload

---
+++ search.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class search
+{
+public static void main(String[] args)
+{
+ArrayList<String> list = new ArrayList<>();
+list.add("Java");
+list.add("Python");
+list.add("C++");
+System.out.println(list.contains("Java"));
+}
+}
