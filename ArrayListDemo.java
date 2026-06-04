6884a434e81138935da27a59d5ef75402d6ffdd5
Rajaprakash R <rajaprakash782005@gmail.com>
2026-06-04 09:44:52 +0530

    Add files via upload

---
+++ ArrayListDemo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class ArrayListDemo
+{
+public static void main(String[] args)
+{
+ArrayList<String> fruits = new ArrayList<>();
+fruits.add("Apple");
+fruits.add("Banana");
+fruits.add("Mango");
+System.out.println(fruits);
+}
+}
