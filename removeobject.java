4d397afd527b9b7761bdba52f1abee5bf87afc8d
Rajaprakash R <rajaprakash782005@gmail.com>
2026-06-04 10:26:22 +0530

    Add files via upload

---
+++ removeobject.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class removeobject
+{
+public static void main(String[] args)
+{
+ArrayList<String> list = new ArrayList<>();
+list.add("Java");
+list.add("Python");
+list.remove("C++");
+System.out.println(list);
+}
+}
